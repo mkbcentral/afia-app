aaed6c668a159e17b146b970cb65085f28b9d4f9
mkbcentral <mkbcentralcode@gmail.com>
2022-10-21 10:11:14 +0200

    Ajouter les fonctions d'ajout et de suppression d'une fiche

--- app/Helpers/Fiches/FicheHelper.php
+++ app/Helpers/Fiches/FicheHelper.php
@@ -4,13 +4,27 @@
 use App\Models\Fiche;
 
     class FicheHelper{
-        //Crezte new fache
+        //Create new fache
         public function create($numero,$type,$source){
             $fiche=Fiche::create([
                 'numero'=>$numero,
                 'type'=>$type,
                 'source'=>$source,
             ]);
+            return $fiche;
+        }
+        //Update fiche
+        public function update($id,$numero,$type,$source){
+            $fiche=Fiche::find($id);
+            $fiche->numero=$numero;
+            $fiche->type=$type;
+            $fiche->source=$source;
+            $fiche->update();
+            return $fiche;
+        }
+        //Delete fiche
+        public function delete($id){
+            $fiche=Fiche::find($id);
             return $fiche;
         }
     }
