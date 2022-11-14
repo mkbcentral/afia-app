211e311e5c768384075c1b0bce11f275ef9c4179
mkbcentral <mkbcentralcode@gmail.com>
2022-11-14 10:18:02 +0200

    Ajouter la fonction pour formater le numero de la facture dans FactureFormatNumber

--- app/Helpers/Facture/FactureFormatNumberHelper.php
+++ app/Helpers/Facture/FactureFormatNumberHelper.php
@@ -1,5 +1,20 @@
 <?php
     namespace App\Helpers\Facture;
-    class FactureFormatNumberHelper{
 
+use App\Models\FacturePrive;
+
+    class FactureFormatNumberHelper{
+        public function formatPrive($month){
+            $factures=FacturePrive::where('month',$month)->get();
+            $number=sprintf('%03d',$factures->count()+1).'.'.$month.'.'.date('y').'PS';
+            return $number;
+        }
+
+        public function formatAbonne(){
+
+        }
+
+        public function formatPersonnel(){
+
+        }
     }
